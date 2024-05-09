c4c83d9dc1654903134cffeef37d273fd53d7bb5
yeeun03030 <skdPdms04@gmail.com>
2024-05-09 14:07:08 +0900

    ViewFlipper ex1

--- app/src/main/java/kr/ac/kopo/viewflippertest/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/viewflippertest/MainActivity.kt
@@ -2,10 +2,32 @@ package kr.ac.kopo.viewflippertest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ViewFlipper
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnNext: Button
+    lateinit var btnPrev: Button
+    lateinit var vFlip: ViewFlipper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnNext = findViewById<Button>(R.id.btnNext)
+        btnPrev = findViewById<Button>(R.id.btnPrev)
+
+        vFlip = findViewById<ViewFlipper>(R.id.flipper)
+
+        btnNext.setOnClickListener(btnListener)
+        btnPrev.setOnClickListener(btnListener)
+    }
+
+    val btnListener = View.OnClickListener {
+        when(it.id) {
+            R.id.btnNext -> vFlip.showNext()
+            R.id.btnPrev -> vFlip.showPrevious()
+        }
     }
 }
